2c4acfa2ba0fc9a63f816f3ee73950bead26a5a4
najibu <ns.najibu@gmail.com>
2017-01-21 14:32:24 +0000

    Added create and show actions

--- app/Accommodation.php
+++ app/Accommodation.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Accommodation extends Model
 {
-    //
+    protected $guarded = ['id'];
 }
--- app/Http/Controllers/AccommodationsController.php
+++ app/Http/Controllers/AccommodationsController.php
@@ -23,7 +23,7 @@ class AccommodationsController extends Controller
      */
     public function create()
     {
-        //
+        return view('accommodations.create');
     }
 
     /**
@@ -45,7 +45,7 @@ class AccommodationsController extends Controller
      */
     public function show($id)
     {
-        return;
+        return Accommodation::findOrFail($id);
     }
 
     /**
